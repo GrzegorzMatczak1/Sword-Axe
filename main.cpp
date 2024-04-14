e8219cff21c532cc8e47b70ffb3388a400b63464
DanilProkopyshen <147425386+DanilProkopyshen@users.noreply.github.com>
2024-04-14 23:08:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,17 @@
 #include <map>
 #include <vector>
 #include <string>
-#include <windows.h>
 #include "item.h"
 #include "inventory.h"
+#include <random>
+
+// for platform specific functionality //  for now only clear() method in Game
+#if defined(_WIN32)
+#define PLATFORM_WINDOWS
+#elif defined(__APPLE__)
+#define PLATFORM_MACOS
+#else
+#endif
 
 
 using namespace std;
@@ -19,18 +27,20 @@ public:
     Items()
     {
         item_list = {
-            Item(1, "Wood", "W", "Resourse", "General", "Common"),
-            Item(1, "Stone", "S", "Resourse", "General", "Common"),
-            Item(1, "Metal", "M", "Resourse", "General", "Uncommon"),
-            Item(1, "Rope", "R", "Resourse", "General", "Uncommon"),
-            Item(1, "Stick", "S", "Resourse", "General", "Common")
+            Item(1, "Wood", "W", "Resourse", "general", "Common"),
+            Item(1, "Stone", "S", "Resourse", "general", "Common"),
+            Item(1, "Metal", "M", "Resourse", "general", "Uncommon"),
+            Item(1, "Rope", "R", "Resourse", "general", "Uncommon"),
+            Item(1, "Stick", "S", "Resourse", "general", "Common")
         };
     }
 
     int getRandomIndex()
     {
-        srand(time(NULL));
-        return rand() % item_list.size();
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<> dis(0, item_list.size() - 1); // nwm jak to działa. ale przynajmniej nie zwraca takich samych wartości jak sie wywoła kilka razy w tej samej sekundzie
+        return dis(gen);
     }
 
     Item getRandomItem()
@@ -40,11 +50,13 @@ public:
 
     void setRandomAmount(int itemIndex) // can be specific or random
     {
-        srand(time(NULL));
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<> dis(1, item_list.size());
 
         for (int i = 0; i < item_list.size(); i++)
         {
-            int randAmount = rand() % item_list.size() + 1;
+            int randAmount = dis(gen);
             item_list[i].amount = randAmount;
         }
     }
@@ -56,12 +68,16 @@ public:
     Inventory* I;
     bool isRunning;
     string currentOperation;
-    string mostRecentOperation;
+    string logsMessage; // logs info //  example: "Swap successful", "Couldn't inspect an item. Wrong input!" etc
     enum class Operations
     {
         Default,
         Swap,
-        Inspect
+        Inspect,
+        Drop,
+
+        Exit,
+        Error
     };
     map<Operations, string> typesOfOperations;
 
@@ -69,6 +85,8 @@ public:
     {
         I = new Inventory();
         isRunning = true;
+        logsMessage = "Welcome!";
+        currentOperation = "default";
 
         // this all will basically be used for displaying specific operations
         // when we select some, the terminal will be cleared and display the specific menu
@@ -78,23 +96,39 @@ public:
         typesOfOperations[Operations::Default] = "default"; // display main menu
         typesOfOperations[Operations::Swap] = "swap";
         typesOfOperations[Operations::Inspect] = "inspect";
+        typesOfOperations[Operations::Drop] = "drop";
+
+        typesOfOperations[Operations::Exit] = "exit";
 
         //we'll probably need to add: delete/disassemble, exit menu, shop, blacksmith, combat(fighting enemies or whatever) and maybe something else
     }
 
     void run()
     {
-        do {
+        while(isRunning){
+            clear();
+
+
+
             gameDisplay();
-        } while(isRunning);
+        }
 
         clear();
+
+        clear();
+
+        cout << endl;
+        cout << "-----------------------------------------" << endl;
         cout << "Thanks for playing!" << endl;
+        cout << "-----------------------------------------" << endl;
     }
 
     void gameDisplay()
     {
-        Operations op = Operations::Default;//getCurrentOperation(currentOperation);
+
+        logsDisplay();
+
+        Operations op = getCurrentOperation(currentOperation);
         switch (op)
         {
         case Operations::Default:
@@ -105,6 +139,16 @@ public:
             break;
         case Operations::Inspect:
             inspectMenu();
+            break;
+        case Operations::Drop:
+            dropMenu();
+            break;
+
+        case Operations::Exit:
+            isRunning = !isRunning;
+            break;
+        case Operations::Error:
+            cout << "Some kind of error!" << endl;
             break;
         default:
             cout << "Some kind of error!" << endl;
@@ -120,12 +164,11 @@ public:
                 return pair.first; // returns a enum operation value // will be used in gameDisplay
             }
         }
+        return Operations::Error;
     }
 
     void mainMenu()
     {
-        clear();
-
         cout << endl << "  Inventory:" << endl;
         I->display();
         cout << endl;
@@ -134,31 +177,67 @@ public:
         cout << "  1. Swap items." << endl;
         cout << "  2. Inspect an item." << endl;
         cout << "  3. Drop an item" << endl;
-        cout << "  4. Exit game." << "\n\n";
+        cout << "  exit. Exit game." << "\n\n";
 
-        cout << "  Input:" << endl;
+        string input;
+        cout << "  Input: ";
+        cin >> input;
+        cout << endl;
 
+        if (input == "1")
+        {
+            currentOperation = "swap";
+        }
+        else if (input == "2")
+        {
+            currentOperation = "inspect";
+        }
+        else if (input == "3")
+        {
+            currentOperation = "drop";
+        }
+
+        else if (input == "exit")
+        {
+            currentOperation = "exit";
+        }
+        else
+        {
+            logsMessage = "Couldn't pick an option. Your input must be invalid! Try typing a number next time!";
+        }
     }
 
     void swapMenu()
     {
-    // nothing yet
+        // nothing yet
     }
 
     void inspectMenu()
     {
-    // work in progress
+        // work in progress
+    }
+
+    void dropMenu()
+    {
+        // nothing there yet
     }
 
     void logsDisplay()
     {
         cout << "-----------------------------------------" << endl;
-        cout << mostRecentOperation << endl;
+        cout << "  " << logsMessage << endl;
+        cout << "-----------------------------------------" << endl;
+        cout << "\n";
     }
 
     void clear()
     {
+        #ifdef PLATFORM_WINDOWS
         system("cls");
+        #elif defined(PLATFORM_MACOS)
+        system("clear");
+        #else
+        #endif
     }
 
     ~Game()
@@ -166,28 +245,24 @@ public:
         delete I;
     }
 };
-/*
 
-*/
 int main()
 {
+
     Game game;
-    game.gameDisplay();
+
+    //game.gameDisplay();
+    game.run();
 
     Inventory I;
+    Items It;
 
-    //I.add_item(2, 4, Item(0, "", "1"));
-    //I.add_item(4, 1, Item(0, "", "2"));
-    //I.add_item(3, 8, Item(0, "", "3"));
-    //I.add_item(1, 4, Item(0, "", "4"));
+    //I.add_item(2, 4, It.getRandomItem());
+    //I.add_item(4, 1, It.getRandomItem());
+    //I.add_item(3, 8, It.getRandomItem());
+    //I.add_item(1, 4, It.getRandomItem());
     //I.add_gear_to_main(1, 1, Boots(0, "", "B"));
     //I.add_gear_to_main(1, 6, Weapon(0, "", "W"));
 
-    //I.display();
-
-    //I.swap_items();
-
-    //I.display();
     return 0;
 }
-
