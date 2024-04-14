89d311d128051bdb9ffa2f9803405a434aeb987b
DanilProkopyshen <147425386+DanilProkopyshen@users.noreply.github.com>
2024-04-14 23:07:17 +0200

    Update item.h

--- item.h
+++ item.h
@@ -9,31 +9,28 @@ class Item
 {
 public:
     int amount;
-    int max_amount = 5;
+    int max_amount;
     string name;
     string display_name;
     string item_type;
     string slot_type;
     string rarity;
 
-
-    Item(int amount = 0, string name = "", string display_name = "", string item_type = "", string slot_type = "", string rarity = "common")
-        : amount{amount}, name{name}, display_name{display_name}, item_type{item_type}, slot_type{slot_type}, rarity{rarity} {}
+    Item(int amount = 0, string name = "", string display_name = "", string item_type = "", string slot_type = "general", string rarity = "common");
 
     virtual void get_data() const;
-
 };
+
 
 class Gear : public Item
 {
 public:
-    int durability;
-    string quality;
+    vector<Item> components;
 
-    Gear(int amount = 0, string name = "", string display_name = "", string item_type = "", string slot_type = "",
-         int durability = 0, string quality = "")
-        : Item(amount, name, display_name, item_type, slot_type) {}
+    Gear(int amount = 0, string name = "", string display_name = "", string item_type = "", string slot_type = "gear", string rarity = "common", vector<Item> components = {});
+
 };
+
 
 class Weapon : public Gear
 {
@@ -42,58 +39,12 @@ public:
     int actual_damage;
     float crit_chance;
 
-    Weapon(int amount = 0, string name = "none", string display_name = "", string item_type = "", string slot_type = "weapon",
-           int durability = 0, string quality = "", int base_damage = 0, float crit_chance = 0.0)
-        : Gear(amount, name, display_name, item_type, slot_type, durability, quality),
+    Weapon(int amount = 0, string name = "", string display_name = "", string item_type = "", string slot_type = "weapon",
+        string rarity = "common", vector<Item> components = {}, int base_damage = 0, float crit_chance = 0.0)
+        : Gear(amount, name, display_name, item_type, slot_type),
         base_damage(base_damage), crit_chance(crit_chance) {}
 
     void get_data() const override;
-
-};
-
-class Armor : public Gear
-{
-public:
-    int base_defence;
-    int actual_defence;
-
-    Armor(int amount = 0, string name = "none", string display_name = "", string item_type = "", string slot_type = "",
-          int durability = 0, string quality = "", int base_defence = 0)
-        : Gear(amount, name, display_name, item_type, slot_type, durability, quality), base_defence(base_defence) {}
-
-    void get_data() const override;
-};
-
-class Helmet : public Armor
-{
-public:
-    Helmet(int amount = 0, string name = "none", string display_name = "", string item_type = "", string slot_type = "helmet",
-           int durability = 0, string quality = "", int base_defence = 0)
-        : Armor(amount, name, display_name, item_type, slot_type, durability, quality, base_defence) {}
-};
-
-class Chestplate : public Armor
-{
-public:
-    Chestplate(int amount = 0, string name = "none", string display_name = "", string item_type = "", string slot_type = "chestplate",
-           int durability = 0, string quality = "", int base_defence = 0)
-        : Armor(amount, name, display_name, item_type, slot_type, durability, quality, base_defence) {}
-};
-
-class Leggins : public Armor
-{
-public:
-    Leggins(int amount = 0, string name = "none", string display_name = "", string item_type = "", string slot_type = "leggins",
-               int durability = 0, string quality = "", int base_defence = 0)
-        : Armor(amount, name, display_name, item_type, slot_type, durability, quality, base_defence) {}
-};
-
-class Boots : public Armor
-{
-public:
-    Boots(int amount = 0, string name = "none", string display_name = "", string item_type = "", string slot_type = "boots",
-               int durability = 0, string quality = "", int base_defence = 0)
-        : Armor(amount, name, display_name, item_type, slot_type, durability, quality, base_defence) {}
 };
 
 class Shield : public Gear
@@ -102,10 +53,58 @@ public:
     int base_defence;
     int actual_defence;
 
-    Shield(int amount = 0, string name = "none", string display_name = "", string item_type = "", string slot_type = "shield",
-           int durability = 0, string quality = "", int base_defence = 0)
-        : Gear(amount, name, display_name, item_type, slot_type, durability, quality), base_defence(base_defence) {}
+    Shield(int amount = 0, string name = "", string display_name = "", string item_type = "", string slot_type = "shield", string rarity = "common", vector<Item> components = {}, int base_defence = 0)
+        : Gear(amount, name, display_name, item_type, slot_type, rarity, components), base_defence(base_defence) {}
 
+    void get_data() const override;
+};
+
+class Armor : public Gear
+{
+public:
+    int base_defence;
+    int actual_defence;
+
+    Armor(int amount = 0, string name = "", string display_name = "", string item_type = "", string slot_type = "armor",
+          string rarity = "common", vector<Item> components = {}, int base_defence = 0)
+        : Gear(amount, name, display_name, item_type, slot_type, rarity, components), base_defence(base_defence) {}
+
+
+};
+
+class Helmet : public Armor
+{
+public:
+    Helmet(int amount = 0, string name = "none", string display_name = "", string item_type = "", string slot_type = "helmet",
+           string rarity = "common", vector<Item> components = {}, int base_defence = 0)
+        : Armor(amount, name, display_name, item_type, slot_type, rarity, components, base_defence) {}
+    void get_data() const override;
+};
+
+class Chestplate : public Armor
+{
+public:
+    Chestplate(int amount = 0, string name = "none", string display_name = "", string item_type = "", string slot_type = "chestplate",
+           string rarity = "common", vector<Item> components = {}, int base_defence = 0)
+        : Armor(amount, name, display_name, item_type, slot_type, rarity, components, base_defence) {}
+    void get_data() const override;
+};
+
+class Leggins : public Armor
+{
+public:
+    Leggins(int amount = 0, string name = "none", string display_name = "", string item_type = "", string slot_type = "leggins",
+               string rarity = "common", vector<Item> components = {}, int base_defence = 0)
+        : Armor(amount, name, display_name, item_type, slot_type, rarity, components, base_defence) {}
+    void get_data() const override;
+};
+
+class Boots : public Armor
+{
+public:
+    Boots(int amount = 0, string name = "none", string display_name = "", string item_type = "", string slot_type = "boots",
+               string rarity = "common", vector<Item> components = {}, int base_defence = 0)
+        : Armor(amount, name, display_name, item_type, slot_type, rarity, components, base_defence) {}
     void get_data() const override;
 };
 
