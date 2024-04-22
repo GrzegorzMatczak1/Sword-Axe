f1864c0282259eaf7b3461af1c5f881b9baccaaf
DanilProkopyshen <147425386+DanilProkopyshen@users.noreply.github.com>
2024-04-22 22:53:31 +0200

    Update item_source.cpp

--- item_source.cpp
+++ item_source.cpp
@@ -1,40 +1,39 @@
 #include <iostream>
 #include "item.h"
+#include "rarity.h"
 
 using namespace std;
 
-Item::Item(int amount, string name, string display_name, string item_type, string slot_type, string rarity)
-    : amount{amount}, name{name}, display_name{display_name}, item_type{item_type}, slot_type{slot_type}, rarity{rarity}
+Item::Item(string name, string display_name, string slot_type, string rarity)
+    :  name{name}, display_name{display_name}, slot_type{slot_type}, rarity{rarity}
 {
-    max_amount = 5;
+    Rarities r;
+
+    //r.applyRarity<Item>(this);
 }
 
-Gear::Gear(int amount, string name, string display_name, string item_type, string slot_type, string rarity,
-           vector<Item> components) : Item(amount, name, display_name, item_type, slot_type, rarity), components(components) {}
+Gear::Gear(string name, string display_name, string slot_type, string rarity,
+           vector<Item> components) : Item(name, display_name, slot_type, rarity), components(components) {}
 
 void Item::get_data() const
 {
     cout << "Name: " << this->name << endl;
-    cout << "Amount: " << this->amount << endl;
-    cout << "Type: " << this->item_type << endl;
+    cout << "Type: " << this->slot_type << endl;
     cout << "Rarity: " << this->rarity << endl;
 }
 
 void Weapon::get_data() const
 {
     cout << "Name: " << this->name << endl;
-    cout << "Type: " << this->item_type << endl;
+    cout << "Type: " << this->slot_type << endl;
     cout << "Rarity: " << this->rarity << endl;
     cout << "Damage: " << this->actual_damage << endl;
-    cout << "Crit chance: " << this->crit_chance << endl;
 }
-
-
 
 void Shield::get_data() const
 {
     cout << "Name: " << this->name << endl;
-    cout << "Type: " << this->item_type << endl;
+    cout << "Type: " << this->slot_type << endl;
     cout << "Rarity: " << this->rarity << endl;
     cout << "Defence: " << this->actual_defence << endl;
 }
@@ -42,28 +41,28 @@ void Shield::get_data() const
 void Helmet::get_data() const
 {
     cout << "Name: " << this->name << endl;
-    cout << "Type: " << this->item_type << endl;
+    cout << "Type: " << this->slot_type << endl;
     cout << "Rarity: " << this->rarity << endl;
     cout << "Defence: " << this->actual_defence << endl;
 }
 void Chestplate::get_data() const
 {
     cout << "Name: " << this->name << endl;
-    cout << "Type: " << this->item_type << endl;
+    cout << "Type: " << this->slot_type << endl;
     cout << "Rarity: " << this->rarity << endl;
     cout << "Defence: " << this->actual_defence << endl;
 }
 void Leggins::get_data() const
 {
     cout << "Name: " << this->name << endl;
-    cout << "Type: " << this->item_type << endl;
+    cout << "Type: " << this->slot_type << endl;
     cout << "Rarity: " << this->rarity << endl;
     cout << "Defence: " << this->actual_defence << endl;
 }
 void Boots::get_data() const
 {
     cout << "Name: " << this->name << endl;
-    cout << "Type: " << this->item_type << endl;
+    cout << "Type: " << this->slot_type << endl;
     cout << "Rarity: " << this->rarity << endl;
     cout << "Defence: " << this->actual_defence << endl;
 }
