a5424323f010029cbddec2f183247b7bf6a4fd9e
DanilProkopyshen <147425386+DanilProkopyshen@users.noreply.github.com>
2024-04-28 22:23:11 +0200

    Update rarity.h

--- rarity.h
+++ rarity.h
@@ -3,25 +3,20 @@
 #include <iostream>
 #include <vector>
 
-class Item;
-class Weapon;
-class Shield;
-class Armor;
-
 using namespace std;
 
 class Rarity
 {
 public:
     string name;
-    float defenceMultiplier;
-    float damageMultiplier;
+    int defenceMultiplier;
+    int damageMultiplier;
+    int cost;
 
-    Rarity(string name, float defenceMultiplier, float damageMultiplier)
-        : name{name}, defenceMultiplier{defenceMultiplier}, damageMultiplier{damageMultiplier} {}
+    Rarity(string name, int damageMultiplier, int defenceMultiplier, int cost)
+        : name{name}, damageMultiplier{damageMultiplier}, defenceMultiplier{defenceMultiplier}, cost{cost} {}
 
     Rarity(string name = "nonexistent"){}
-
 
 };
 
@@ -33,11 +28,11 @@ public:
     Rarities()
     {
         rarities = {
-            Rarity("Common", 0.0, 0.0),
-            Rarity("Uncommon", 0.075, 0.075),
-            Rarity("Rare", 0.15, 0.15),
-            Rarity("Epic", 0.225, 0.225),
-            Rarity("Legendary", 0.4, 0.4)
+            Rarity("Common", 0, 0, 0),
+            Rarity("Uncommon", 2, 2, 15),
+            Rarity("Rare", 3, 3, 25),
+            Rarity("Epic", 5, 5, 35),
+            Rarity("Legendary", 7, 7, 50)
         };
     }
 
@@ -52,21 +47,21 @@ public:
         }
         return Rarity();
     }
-/*
-    template<typename T>
+
+    template <typename T>
     void applyRarity(T* item)
     {
 
-
-        if (is_same<T, Weapon>::value)
+        if (item->slot_type == "weapon")
         {
-            item->actual_damage = int(item->base_damage + (item->base_damage * getRarity(item->rarity).damageMultiplier));
-        } else if (is_same<T, Shield>::value || is_same<T, Armor>::value)
+            item->actual_damage = item->base_damage + getRarity(item->rarity).damageMultiplier;
+        } else if (item->slot_type == "general") {}
+        else
         {
-            item->actual_defence = int(item->base_defence + (item->base_defence * getRarity(item->rarity).defenceMultiplier));
+            item->actual_defence = item->base_defence + getRarity(item->rarity).defenceMultiplier;
         }
     }
-*/
+
 };
 
 #endif // RARITY_H
