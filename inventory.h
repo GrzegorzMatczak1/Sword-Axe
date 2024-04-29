71fcd65a5ab1efc03bf064c6355c20f7787e962e
DanilProkopyshen <147425386+DanilProkopyshen@users.noreply.github.com>
2024-04-30 00:31:22 +0200

    Update inventory.h

--- inventory.h
+++ inventory.h
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-class Inventory //Inventory Class
+class Inventory
 {
 public:
     Item*** tab;
@@ -18,38 +18,33 @@ public:
     map<char, int> row_indexes;
     map<string, int> gear_slots;
 
-
-    Inventory(int rows = 4, int cols = 9); //Inventory Constructor
-
-
-    // this one creates a map (dictionary) of letters and their matching indexes
-    // depending on amound of rows. example: 'A': 0, 'B': 1 and so on until the amout is equeal to rows
+    Inventory(int rows = 4, int cols = 9);
+    
+    
     void fill_row_indexes();
 
 
-    // indexes of gear slots // used in swap items
     void fill_gear_slots();
 
 
-    // is used to return a number value that corelates to an Item slot type in gear_slots
     int get_gear_slot_index(string item_slot_type);
-
-
-    // displays out inventory with every item that's in there
+    
+    
     void display();
 
 
     void displayGold();
-
+    
+    
     void increaseGold(int amount);
-
+    
+    
     void decreaseGold(int amount);
-
-
-    // adds an item to the inventory // user won't be able to specify where the items will be added because he sohouldn't be able to
+    
+    
     void add_item(int row_cords, int col_cords, const Item& itemToAdd);
-
-    // returns processed cord
+    
+    
     auto get_processed_cords(string cords) -> vector<int>;
 
 
@@ -66,17 +61,19 @@ public:
 
 
     vector<int> getFirstEmptySlot();
-    
+
 
     string upgradeAnItem(string cords, int* playerGold);
 
 
     string disassembleAnItem(string cords);
-    
-    
+
+
     void displayItemComponents(string cords);
-    
+
     string validItem(string cords);
+
+    string sellAnItem(string cords);
 
 
     ~Inventory();
